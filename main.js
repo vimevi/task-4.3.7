9425b184582496374df26415be3c653e052830b3
Maxim Toropchin <vimevi57@gmail.com>
2023-11-01 17:43:58 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,114 @@
+const container = document.querySelector('.container');
+
+// Создание формы с инпутом
+const form = document.createElement('form');
+const input = document.createElement('input');
+
+form.appendChild(input);
+container.appendChild(form);
+input.classList.add('search-repos__input');
+input.placeholder = 'Введите запрос..';
+input.setAttribute('type', 'text');
+input.setAttribute('name', 'name');
+
+// убирает обновление страницы при нажатии на enter
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+});
+
+// Создание autocomplete поля
+const searchResults = document.createElement('div');
+
+const ul = document.createElement('ul');
+
+searchResults.classList.add('search-repos__results');
+container.appendChild(searchResults);
+searchResults.appendChild(ul);
+let itemCount = 0;
+
+// создание списка репозиториев
+async function generateRepoInfo(repoData) {
+	if (itemCount < 5) {
+		const repoList = document.createElement('div');
+		const delBtn = document.createElement('button');
+		const repoInfo = document.createElement('ul');
+		const repoName = document.createElement('li');
+		const repoOwner = document.createElement('li');
+		const repoStars = document.createElement('li');
+
+		repoList.classList.add('search-repos__list');
+		repoName.textContent = `Name: ${repoData.name}`;
+		repoOwner.textContent = `Owner: ${repoData.owner.login}`;
+		repoStars.textContent = `Stars: ${repoData.stargazers_count}`;
+
+		container.appendChild(repoList);
+		repoList.appendChild(repoInfo);
+		repoInfo.appendChild(repoName);
+		repoInfo.appendChild(repoOwner);
+		repoInfo.appendChild(repoStars);
+
+		repoInfo.appendChild(delBtn);
+		delBtn.classList.add('search-repos__del-btn');
+
+		// слушатель события удаления
+		delBtn.addEventListener('click', (e) => {
+			e.preventDefault();
+			repoList.remove();
+			itemCount--;
+		});
+		itemCount++;
+	}
+}
+
+const debounce = (fn, debounceTime) => {
+	let timeout;
+	return function () {
+		const fnCall = () => {
+			fn.apply(this, arguments);
+		};
+		// отмена setTimeout если не прошло debounceTime
+		clearTimeout(timeout);
+
+		timeout = setTimeout(fnCall, debounceTime);
+	};
+};
+
+async function onChange(e) {
+	e.preventDefault();
+
+	const inputsValue = Object.fromEntries(new FormData(e.target.form));
+
+	const response = await fetch(
+		`https://api.github.com/search/repositories?q=${inputsValue.name}`
+	);
+	if (response.ok) {
+		const data = await response.json();
+		console.log(data); // Check the data in the console
+		renderResults(data);
+	} else {
+		// очистка автозаполнения
+		renderResults();
+		console.log('repo is not found');
+	}
+}
+
+function renderResults(repoData) {
+	ul.innerHTML = '';
+	// ограничение кол-ва результатов запроса
+	const numResults = Math.min(5, repoData.items.length);
+
+	for (let i = 0; i < numResults; i++) {
+		const item = repoData.items[i];
+		const li = document.createElement('li');
+		li.textContent = item.name; // Set the text content of the li element
+		ul.appendChild(li); // Append the li element to the ul
+		li.addEventListener('click', () => {
+			generateRepoInfo(item);
+		});
+	}
+	searchResults.appendChild(ul);
+}
+
+function createDeleteBtnEl() {}
+
+input.addEventListener('keyup', debounce(onChange, 350)); // опытным путём выведено значение 350 мс
